19210598c13cec3193577fb4e0a6269fe3b366e1
Ahmed Salah <ahmedsalahalanani@gmail.com>
2020-09-30 19:04:21 +0200

    Add files via upload

---
+++ Debug/src/DigitalClock_program.d
@@ -0,0 +1,21 @@
+src/DigitalClock_program.o: ../src/DigitalClock_program.c \
+ ../include/STD_TYPES.h ../include/BIT_MATH.h ../include/NVIC_interface.h \
+ ../include/Timer2_5_interface.h ../include/TFT_interface.h \
+ ../include/DigitalClock_interface.h ../include/DigitalClock_config.h \
+ ../include/DigitalClock_private.h
+
+../include/STD_TYPES.h:
+
+../include/BIT_MATH.h:
+
+../include/NVIC_interface.h:
+
+../include/Timer2_5_interface.h:
+
+../include/TFT_interface.h:
+
+../include/DigitalClock_interface.h:
+
+../include/DigitalClock_config.h:
+
+../include/DigitalClock_private.h:
